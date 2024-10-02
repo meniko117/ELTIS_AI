f47065ddff3cdc3640db85820318c61baded5c2e
Maxim Smirnov <m_smirnov@.yahoo.com>
2024-10-02 21:16:25 +0300

    add Llama parsing

---
+++ rag_manager_app/llama_parse.py
@@ -0,0 +1,65 @@
+# bring in our LLAMA_CLOUD_API_KEY
+from dotenv import load_dotenv
+load_dotenv()
+
+# bring in deps
+from llama_parse import LlamaParse
+from llama_index.core import SimpleDirectoryReader
+
+from dotenv import load_dotenv
+import os
+
+import nest_asyncio
+nest_asyncio.apply()
+
+# https://cloud.llamaindex.ai/parse
+
+LLAMA_CLOUD_API_KEY = LLAMA_CLOUD_API_KEY
+
+
+# Load variables from .env file
+load_dotenv()
+
+api_key = os.getenv('LLAMA_CLOUD_API_KEY')
+print(api_key)  # Output will
+
+
+# Set API key
+os.environ['LLAMA_CLOUD_API_KEY'] = LLAMA_CLOUD_API_KEY
+
+# Access the API key
+api_key = os.getenv('LLAMA_CLOUD_API_KEY')
+print(api_key)  # Output will be your API key
+
+
+
+# set up parser
+parser = LlamaParse(
+    result_type="markdown"  # "markdown" and "text" are available
+)
+
+# use SimpleDirectoryReader to parse our file
+file_extractor = {".pdf": parser}
+documents = SimpleDirectoryReader(input_files=[r"C:\Users\134\saved_pages\ELTIS_site\download\reh-eltis-dpu5000-kedc-setevoj.pdf"], file_extractor=file_extractor).load_data()
+print(documents)
+
+
+output_folder = r"C:\Users\134\Documents\for_Llama_index"
+output_file = os.path.join(output_folder, "dpu5000_kedc_markdown.md")
+
+# Initialize an empty string to hold the concatenated markdown content
+all_markdown_content = ""
+
+# Iterate over the list of documents and concatenate their content
+for doc in documents:
+    # Access the content of the document correctly
+    markdown_content = doc.text  # Adjust according to the actual attribute
+    
+    # Concatenate the markdown content
+    all_markdown_content += markdown_content + "\n\n"  # Add newlines to separate the content
+
+# Save the concatenated markdown content to a single file
+with open(output_file, 'w', encoding='utf-8') as f:
+    f.write(all_markdown_content)
+
+print(f"Markdown content saved successfully to {output_file}.")
